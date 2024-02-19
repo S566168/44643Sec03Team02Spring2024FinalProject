a1ad34ac6dd74701338e0eaee2255b07b8b097b5
Akhil Reddy Depa <143060945+Akhil2402@users.noreply.github.com>
2024-02-18 19:39:33 -0600

    Add files via upload

---
+++ 44643Sec03Team02Spring2024FinalProject/RealVision/RealVision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealVision
+//
+//  Created by AKHIL REDDY on 18/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 44643Sec03Team02Spring2024FinalProject/RealVision/RealVisionUITests/RealVisionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RealVisionUITestsLaunchTests.swift
+//  RealVisionUITests
+//
+//  Created by AKHIL REDDY on 18/02/24.
+//
+
+import XCTest
+
+final class RealVisionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
