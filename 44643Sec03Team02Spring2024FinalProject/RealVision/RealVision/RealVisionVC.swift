bb8d77bfad9df4bb5ed5cea1396c147aa40c76be
Udaykiranreddy <S567161@nwmissouri.edu>
2024-02-20 01:05:37 -0600

    Udaykiranreddy Devarapally: Added Outlets and Actions

---
+++ 44643Sec03Team02Spring2024FinalProject/RealVision/RealVision/RealVisionVC.swift
@@ -0,0 +1,52 @@
+//
+//  RealVisionVC.swift
+//  RealVision
+//
+//  Created by S567161 on 2/20/24.
+//
+
+import UIKit
+import Lottie
+
+class RealVisionVC: UIViewController {
+    
+    @IBOutlet weak var userNameLBL: UILabel!
+    @IBOutlet weak var passwordLBL: UILabel!
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var launchLAV: LottieAnimationView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func userTF(_ sender: UITextField) {
+    }
+    
+    @IBAction func passwordTF(_ sender: UITextField) {
+    }
+    
+    @IBAction func loginBTN(_ sender: UIButton) {
+    }
+    
+    @IBAction func forgotPwdBTN(_ sender: UIButton) {
+    }
+    
+    @IBAction func signUpBTN(_ sender: UIButton) {
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
