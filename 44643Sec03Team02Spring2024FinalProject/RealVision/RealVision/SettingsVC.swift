a86f19a16b7ef4539d2305e10e94aa1450d44141
Udaykiranreddy <S567161@nwmissouri.edu>
2024-03-07 22:56:27 -0600

    Udaykiranreddy Devarapally: Added Cocoa Touch class of settings and added Outlets,Actions and Constraints of Screen 5.

---
+++ 44643Sec03Team02Spring2024FinalProject/RealVision/RealVision/SettingsVC.swift
@@ -0,0 +1,51 @@
+//
+//  settingsVC.swift
+//  RealVision
+//
+//  Created by S567161 on 3/7/24.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+    
+    
+    @IBOutlet weak var shareBTN: UIButton!
+    @IBOutlet weak var helpBTN: UIButton!
+    @IBOutlet weak var privacyBTN: UIButton!
+    @IBOutlet weak var termsBTN: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBAction func share(_ sender: UIButton) {
+    }
+    
+    @IBAction func help(_ sender: UIButton) {
+    }
+    
+    @IBAction func privacy(_ sender: UIButton) {
+    }
+    
+    @IBAction func terms(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
