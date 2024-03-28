d5217a43d3d0303bace20ac4822070c0ad0a8625
Udaykiranreddy <S567161@nwmissouri.edu>
2024-03-27 23:31:30 -0500

    Udaykiranreddy: Sprint05-Commit 1- Added Functionality to the Forgot password and reset the password.

--- 44643Sec03Team02Spring2024FinalProject/RealVision/RealVision/ResetpasswordVC.swift
+++ 44643Sec03Team02Spring2024FinalProject/RealVision/RealVision/ResetpasswordVC.swift
@@ -20,16 +20,53 @@ class ResetpasswordVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        resetBTN.isEnabled = false
+        emailIdTF.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+
     }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+            if let email = emailIdTF.text, isValidEmail(email: email) {
+                resetBTN.isEnabled = true
+            } else {
+                resetBTN.isEnabled = false
+            }
+        }
+    func isValidEmail(email: String) -> Bool {
+            let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+            let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+            return emailPredicate.evaluate(with: email)
+        }
+
     
     @IBAction func email(_ sender: UITextField) {
     }
     
     @IBAction func reset(_ sender: UIButton) {
+        guard let email = emailIdTF.text else {
+                    return
+                }
+                
+        showAlert(message: "Password reset link sent to \(email)"){
+            self.performSegue(withIdentifier: "LoginView", sender: sender)
+        }
+
     }
     @IBAction func Cancel(_ sender: UIButton) {
         self.performSegue(withIdentifier: "LoginView", sender: sender)
     }
+    
+    
+    func showAlert(message: String, completion: @escaping () -> Void) {
+            let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+                
+                completion()
+            }
+            alertController.addAction(okAction)
+            present(alertController, animated: true, completion: nil)
+        }
+
     
     /*
     // MARK: - Navigation
